6dc4a5b59213fa4b0d75e0b655ca349d214fdce4
Joao Garcia <jpmldg@gmail.com>
2024-04-17 13:35:22 +0530

    Testing mdx

--- app/page.js
+++ app/page.js
@@ -1,146 +1,122 @@
-import styles from "./page.module.css";
-import { Card, Stack, Image, Heading, CardBody, Text, CardFooter, Button, extendTheme } from "@chakra-ui/react";
+'use client'
+ 
 
-export default function Home() {
-  const cardui = {} 
+import {
+  Box,
+  Flex,
+  Avatar,
+  Text,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Image
+} from '@chakra-ui/react'
+
+const NavLink = (props) => {
+  const { children } = props
+
   return (
-    <Stack  width="1000px" alignItems="center" mt="80px" margin={"80px auto auto auto"} spacing="0">
-      <Card
-      direction={{ base: 'column', sm: 'row' }}
-      alignItems="end"
-      overflow='hidden'
-      variant='outline'
-      height={"400px"}
-      width="100%"
-      backgroundRepeat={"no-repeat"}
-      backgroundSize="cover"
-      backgroundImage={"linear-gradient(180deg, #ffffff00, #000000da), url('https://raw.githubusercontent.com/Mugen-Builders/playground-frontend/main/assets/chapter_images/chapter_0.webp')"}
-      backgroundPosition="right 50% bottom -50px"
-      color="white"
-      borderRadius={"0"}
-      border="0px solid black"
-      margin="0"
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+      }}
+      href={'#'}>
+      {children}
+    </Box>
+  )
+}
+
+export default function Nav() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Box 
+      backgroundColor={"#232931"} 
+      px={4}
+      filter={"drop-shadow(0 0 0.25rem black)"}
       >
-      <Stack>
-        <CardBody>
-          <Heading size='md'>Chapter I:</Heading>
-          <Text py='2' fontFamily={"Arial"} fontWeight="800" fontSize={"48"} lineHeight="1.1">
-            Enter Cartesia World
-          </Text>
-        </CardBody>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Box>Logo</Box>
 
-      </Stack>
-    </Card>
-
-    <Card
-      direction={{ base: 'column', sm: 'row' }}
-      alignItems="end"
-      overflow='hidden'
-      variant='outline'
-      height={"400px"}
-      width="100%"
-      backgroundRepeat={"no-repeat"}
-      backgroundSize="cover"
-      backgroundImage={"url('https://github.com/Mugen-Builders/playground-frontend/blob/main/assets/chapter_images/ch_2.png?raw=true')"}
-      backgroundPosition="right 50% bottom -50px"
-      color="white"
-      borderRadius={"0"}
-      border="0px solid black"
-      margin="0"
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                  />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+      <Box 
+        display={"flex"}
+        justifyContent={'center'}
+        alignItems={'center'}
+        p={4}
+        height={"120px"} 
+        backgroundSize="cover"
+        boxShadow={"inset 0 0 0 2000px rgba(0, 0, 0, 0.85)"}
+        backgroundPosition={"center bottom -70%"}
+        backgroundImage={"url('https://raw.githubusercontent.com/Mugen-Builders/playground-frontend/main/assets/chapter_images/chapter_0.webp')"}
       >
-      <Stack>
-        <CardBody>
-          <Heading size='md'>Chapter II:</Heading>
-          <Text py='2' fontFamily={"Arial"} fontWeight="800" fontSize={"48"} lineHeight="1.1">
-            The start of the adventure
-          </Text>
-        </CardBody>
-
-      </Stack>
-    </Card>
-
-    <Card
-      direction={{ base: 'column', sm: 'row' }}
-      alignItems="end"
-      overflow='hidden'
-      variant='outline'
-      height={"400px"}
-      width="100%"
-      backgroundRepeat={"no-repeat"}
-      backgroundSize="cover"
-      backgroundImage={"linear-gradient(180deg, #ffffff00, #000000da), url('https://raw.githubusercontent.com/Mugen-Builders/playground-frontend/main/assets/chapter_images/chapter_2.webp')"}
-      backgroundPosition="right 50% bottom -50px"
-      color="white"
-      borderRadius={"0"}
-      border="0px solid black"
-      margin="0"
-      >
-      <Stack>
-        <CardBody>
-          <Heading size='md'>Chapter III:</Heading>
-          <Text py='2' fontFamily={"Arial"} fontWeight="800" fontSize={"48"} lineHeight="1.1">
-            The hoard, the vault, the prize
-          </Text>
-        </CardBody>
-
-      </Stack>
-    </Card>
-
-    <Card
-      direction={{ base: 'column', sm: 'row' }}
-      alignItems="end"
-      overflow='hidden'
-      variant='outline'
-      height={"400px"}
-      width="100%"
-      backgroundRepeat={"no-repeat"}
-      backgroundSize="cover"
-      backgroundImage={"linear-gradient(180deg, #ffffff00, #000000da), url('https://raw.githubusercontent.com/Mugen-Builders/playground-frontend/main/assets/chapter_images/chapter_3.webp')"}
-      backgroundPosition="right 50% bottom -50px"
-      color="white"
-      borderRadius={"0"}
-      border="0px solid black"
-      margin="0"
-      >
-      <Stack>
-        <CardBody>
-          <Heading size='md'>Chapter IV:</Heading>
-          <Text py='2' fontFamily={"Arial"} fontWeight="800" fontSize={"48"} lineHeight="1.1">
-            Exchanges in the grand bazar
-          </Text>
-        </CardBody>
-
-      </Stack>
-    </Card>
-
-    <Card
-      direction={{ base: 'column', sm: 'row' }}
-      alignItems="end"
-      overflow='hidden'
-      variant='outline'
-      height={"400px"}
-      width="100%"
-      backgroundRepeat={"no-repeat"}
-      backgroundSize="cover"
-      backgroundImage={"linear-gradient(180deg, #ffffff00, #000000da), url('https://raw.githubusercontent.com/Mugen-Builders/playground-frontend/main/assets/chapter_images/chapter_4.webp')"}
-      backgroundPosition="right 50% bottom -50px"
-      color="white"
-      borderRadius={"0"}
-      border="0px solid black"
-      margin="0"
-      >
-      <Stack>
-        <CardBody>
-          <Heading size='md'>Chapter V:</Heading>
-          <Text py='2' fontFamily={"Arial"} fontWeight="800" fontSize={"48"} lineHeight="1.1">
-          Interacting with other worlds
-          </Text>
-        </CardBody>
-
-      </Stack>
-    </Card>
+          <Image 
+            height={"90px"}
+            src='https://github.com/Mugen-Builders/playground-frontend/blob/main/assets/character/Subject.png?raw=true' 
+            alt='hero' 
+          />
 
 
-    </Stack>
-  );
+
+      </Box>
+      <Box display={"flex"}>
+        <Box>
+          {/* <MDXProvider components={components}>
+            <Post />
+          </MDXProvider> */}
+        </Box>
+        <Box></Box>
+
+      </Box>
+
+    </>
+  )
 }
