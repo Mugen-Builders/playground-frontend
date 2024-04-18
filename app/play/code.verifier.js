d8e354202c1cbad35730a5ff3116fa86da0b7b24
Joao Garcia <jpmldg@gmail.com>
2024-04-19 03:01:16 +0530

    dinamically executing functions with override ready

--- app/play/code.verifier.js
+++ app/play/code.verifier.js
@@ -4,15 +4,15 @@ import {
 } from "./test.functions";
 
 function executeFunction(functionString) {
+
     try {
-        const fn = new Function('return ' + functionString)();
+        let fn = new Function('return ' + functionString)();
         fn(3,4)
         console.log(`Result: ${fn(1, 2)}`);  // Example execution with dummy data
     } catch (error) {
         console.log(`Error: ${error.message}`);
     }
 }
-
 
 function removeNonUsedCode(codeString) {
     const regex = /\/\* Do not change anything above this line \*\/\s*([\s\S]*?)\s*\/\* Do not change anything below this line \*\//;
@@ -21,17 +21,32 @@ function removeNonUsedCode(codeString) {
     return content
 }
 
-function createDynamicFunction(functionString) {
-    console.log(":: Created dynamic function")
-    console.log(functionString)
-
-    try {
-        const fn = new Function('return ' + functionString)();
-        return fn
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-    }
-}
+// This returns a function that allows you to override params 
+// let foo = createDynamicFunctionBuilder(functionString)
+// will produce a function let bar = foo({wordToOverride : 3})
+// bar(x,y,z) // regular function script call
+function createDynamicFunctionBuilder(functionString) {
+    return function(overrides, ...args) {
+      let overridesCode = "";
+      for (const key in overrides) {
+        if (typeof overrides[key] === 'function') {
+            overridesCode += `let ${key} = ${overrides[key].toString()};\n`;
+        } else {
+            overridesCode += `let ${key} = ${JSON.stringify(overrides[key])};\n`;
+        }
+      }
+  
+      const fullFunctionCode = overridesCode + "return " + functionString;
+  
+      try {
+        const dynamicFn = eval(`(function() { ${fullFunctionCode} })`);
+        return dynamicFn.apply(this, args);
+      } catch (error) {
+        console.error(`Error executing function: ${error.message}`);
+        return null;
+      }
+    };
+  }
 
 async function runTests(functionName, functionReference) {
     console.log(":: Running Tests")
@@ -53,10 +68,9 @@ async function runTests(functionName, functionReference) {
 
 async function test(functionName, fullCodeString) {
     let codeString = removeNonUsedCode(fullCodeString)
-    let functionReference = createDynamicFunction(codeString)
-    await runTests(functionName, functionReference)
+    let generator = createDynamicFunctionBuilder(codeString)
+    await runTests(functionName, generator)
 }
-
 
 export {
     test
--- app/play/test.functions.js
+++ app/play/test.functions.js
@@ -1,6 +1,14 @@
 import { assert } from "./assert";
 
-async function testAdd(functionReference) {
+let fetch = () => {
+    console.log("--- fetch from scope 2 ---")
+}
+
+let rollup_server = "--- rollupserver from scope 2 ---"
+
+async function testAdd(functionGenerator) {
+    let functionReference = functionGenerator()
+    
     assert(functionReference(1,3) == 4, "1,3 should be equal to 4")
     assert(functionReference(10,13) == 23, "10,13 should be equal to 23")
     assert(functionReference(0,0) == 0, "0,0 should be equal to 0")
@@ -8,19 +16,21 @@ async function testAdd(functionReference) {
     console.log("test passed")
 }
 
-async function testFetchOverride(functionReference) {
-    var fetch = async (str, body) => {
+async function testFetchOverride(functionGenerator) {
+    const fetch = async (str, body) => {
         console.log("deu override no fetch")
         console.log(str, body)
         return 1
     }
-    var rollup_server = "potato"    
-    
-    assert(await functionReference() == 1, "1,3 should be equal to 4")
 
-    
-    
+    let rollup_server = 'http://example5.com'
+    let intt = 1
+    let functionReference = functionGenerator({fetch, rollup_server, intt })
 
+    console.log("::: test fetch override")
+    let a = await functionReference(1,2)
+    console.log(a)
+    
 }
  
 
