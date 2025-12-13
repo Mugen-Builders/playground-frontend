91aa0845a0c985609eff504216a3e92083fe33d0
Joao Garcia <jpmldg@gmail.com>
2025-12-13 19:04:55 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 import createMDX from '@next/mdx'
  
+const repo = "playground-frontend";
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
 }
  
 const withMDX = createMDX({
